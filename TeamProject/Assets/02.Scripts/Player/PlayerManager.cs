a9208c9f7e56a87a91c012787cfe43e3d8dcd2be
ljs9711 <32507881+ljs9711@users.noreply.github.com>
2022-07-27 16:34:56 +0900

    player fall , landng 추가
    fall , landing 애니메이션 추가

--- TeamProject/Assets/02.Scripts/Player/PlayerManager.cs
+++ TeamProject/Assets/02.Scripts/Player/PlayerManager.cs
@@ -15,6 +15,8 @@ namespace SG
 
         [Header("Player Flags")]
         public bool isSprinting;
+        public bool isInAir;
+        public bool isGrounded;
         public bool canDoCombo;
 
         private void Awake()
@@ -38,6 +40,7 @@ namespace SG
             inputHandler.TickInput(delta);
             playerLocomotion.HandleMovement(delta);
             playerLocomotion.HandleRollingAndSprinting(delta);
+            playerLocomotion.HandleFalling(delta, playerLocomotion.moveDirection);
     }
 
     private void FixedUpdate()
@@ -57,6 +60,11 @@ namespace SG
             inputHandler.sprintFlag = false;
             inputHandler.rb_Input = false;
             inputHandler.rt_Input = false;
+
+            if(isInAir)
+            {
+                playerLocomotion.inAirTimer = playerLocomotion.inAirTimer + Time.deltaTime;
+            }
         }
     }
 }
