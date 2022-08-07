9c9d9364f24fd61b4c99e16713000ba19e1cf600
fufkgj <homin0928@gmail.com>
2022-08-07 23:43:07 +0900

    BGM 작업중

---
+++ TeamProject/Assets/02.Scripts/Common/BGMController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BGMController : MonoBehaviour
+{
+    Collider BGMCollider;
+    [SerializeField]
+    private AudioClip BGMClips;
+    [SerializeField]
+    private AudioSource audioSource;
+    void Start()
+    {
+        BGMCollider = GetComponent<Collider>();
+        audioSource = GetComponent<AudioSource>();
+        BGMClips = Resources.Load<AudioClip>("BGM/Normal/FieldBGM");
+        audioSource.Play();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (SceneManager.GetActiveScene().name == "Field")
+        {
+            if (other.tag == "Enemy")
+            {
+                BGMClips = Resources.Load<AudioClip>("BGM/Battle/BattleBGM");
+                audioSource.Play();
+            }
+        }
+    }
+}
