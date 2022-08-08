4781741d06eae40eef2e35982f7f7026eba57694
Jerryteacher <97865468+Jerryteacher@users.noreply.github.com>
2022-08-08 10:00:00 +0900

    Villige_NPC수정
    NPC - npc name, speech bubble UI 말풍선 추가 (스크립트는 따로 필요)
    
    NPC - gaurd  추가

---
+++ TeamProject/Assets/02.Scripts/Common/LookAtCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCam : MonoBehaviour
+{
+    private Transform tr;
+    private Transform mainCameraTr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        mainCameraTr = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        tr.LookAt(mainCameraTr);
+    }
+}
