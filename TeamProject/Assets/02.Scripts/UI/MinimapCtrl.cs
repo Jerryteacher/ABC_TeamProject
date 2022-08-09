1d28703e0021f795576100e041bd70f1c25162ff
SMG9 <asnmfg@naver.com>
2022-08-09 16:54:09 +0900

    미니맵 수정
    미니맵 보완 및 수정

---
+++ TeamProject/Assets/02.Scripts/UI/MinimapCtrl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SG;
+
+public class MinimapCtrl : MonoBehaviour
+{
+    Transform camTr;
+    Transform targetTr;
+    [SerializeField] RectTransform miniMap;
+    [SerializeField] RectTransform playerIcon;
+
+    public bool isRotIcon = true;
+
+    readonly string targetTag = "PLAYER";
+    public Transform TargetTr
+    {
+        get
+        {
+            if (targetTr == null)
+                targetTr = GameObject.FindWithTag(targetTag).GetComponent<Transform>();
+            return targetTr;
+        }
+    }
+
+    public Transform CamTr
+    {
+        get
+        {
+            if (camTr == null)
+                camTr = FindObjectOfType<CameraHandler>().GetComponent<Transform>();
+            return camTr;
+        }
+    }    
+
+    private void Start()
+    {
+        miniMap = transform.GetChild(0).GetChild(0).GetComponent<RectTransform>();
+        playerIcon = transform.GetChild(0).GetChild(1).GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(isRotIcon)
+        {
+            miniMap.eulerAngles = new Vector3(0, 0, CamTr.rotation.eulerAngles.y);
+            playerIcon.eulerAngles = new Vector3(0, 0, CamTr.rotation.eulerAngles.y - TargetTr.rotation.eulerAngles.y);
+        }
+        else
+        {
+            miniMap.eulerAngles = Vector3.zero;
+            playerIcon.eulerAngles = new Vector3(0, 0, -TargetTr.rotation.eulerAngles.y);
+        }
+        
+        //float _rot = TargetTr.rotation.eulerAngles.y;
+        //if (isRotIcon)
+        //{
+        //    playerIcon.eulerAngles = new Vector3(0, 0, -_rot);
+        //    miniMap.eulerAngles = Vector3.zero;
+        //}
+        //else
+        //{
+        //    playerIcon.eulerAngles = Vector3.zero;
+        //    float _tmp = CamTr.rotation.eulerAngles.y;
+        //    miniMap.eulerAngles = new Vector3(0, 0, _tmp);
+        //}
+    }
+}
