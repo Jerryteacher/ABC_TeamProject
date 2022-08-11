8f62e28751225b3dc64d77da3f264305489d942b
fufkgj <homin0928@gmail.com>
2022-08-11 10:41:04 +0900

    CameraHandler 수정

--- TeamProject/Assets/02.Scripts/Player/Camera/CameraHandler.cs
+++ TeamProject/Assets/02.Scripts/Player/Camera/CameraHandler.cs
@@ -3,90 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    public class CameraHandler : MonoBehaviour
+public class CameraHandler : MonoBehaviour
+{
+    public Transform targetTransform;
+    public Transform cameraTransform;
+    public Transform cameraPivotTransform;
+    private Transform myTransform;
+    private Vector3 cameraTransformPosition;
+    private LayerMask ignoreLayers;
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+
+    public static CameraHandler singleton;
+
+    public float lookSpeed = 0.1f;
+    public float followSpeed = 0.1f;
+    public float pivotSpeed = 0.03f;
+
+    private float targetPosition;
+    private float defaultPosition;
+    private float lookAngle;
+    private float pivotAngle;
+    public float minimumPivot = -35;
+    public float maximumPivot = 35;
+
+    public float cameraSphereRadius = 0.2f;
+    public float cameraCollisionOffset = 0.2f;
+    public float minimumCollisionOffset = 0.2f;
+    private static CameraHandler instance = null;
+    private void Awake()
     {
-        public Transform targetTransform;
-        public Transform cameraTransform;
-        public Transform cameraPivotTransform;
-        private Transform myTransform;
-        private Vector3 cameraTransformPosition;
-        private LayerMask ignoreLayers;
-        private Vector3 cameraFollowVelocity = Vector3.zero;
-
-        public static CameraHandler singleton;
-
-        public float lookSpeed = 0.1f;
-        public float followSpeed = 0.1f;
-        public float pivotSpeed = 0.03f;
-
-        private float targetPosition;
-        private float defaultPosition;
-        private float lookAngle;
-        private float pivotAngle;
-        public float minimumPivot = -35;
-        public float maximumPivot = 35;
-
-        public float cameraSphereRadius = 0.2f;
-        public float cameraCollisionOffset = 0.2f;
-        public float minimumCollisionOffset = 0.2f;
-
-        private void Awake()
+        if (instance == null)
         {
-            singleton = this;
-            myTransform = transform;
-            defaultPosition = cameraTransform.localPosition.z;
-            ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
-            targetTransform = FindObjectOfType<PlayerManager>().transform;
+            instance = this;
         }
-
-        public void FollowTarget(float delta) //카메라가 플레이어를 따라감
+        else if (instance != this)
         {
-            //Vector3 targetPosition = Vector3.Lerp(myTransform.position, targetTransform.position, delta / followSpeed);
-            Vector3 targetPosition = Vector3.SmoothDamp(myTransform.position, targetTransform.position, ref cameraFollowVelocity, delta / followSpeed);
-            myTransform.position = targetPosition;
-
-            HandleCameraCollision(delta);
+            Destroy(gameObject);
         }
-
-        public void HandleCameraRotation(float delta, float mouseXInput, float mouseYInput) //카메라 회전
-        {
-            lookAngle += (mouseXInput * lookSpeed) / delta;
-            pivotAngle -= (mouseYInput * pivotSpeed) / delta;
-            pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
-
-            Vector3 rotation = Vector3.zero;
-            rotation.y = lookAngle;
-            Quaternion targetRotation = Quaternion.Euler(rotation);
-            myTransform.rotation = targetRotation;
-
-            rotation = Vector3.zero;
-            rotation.x = pivotAngle;
-
-            targetRotation = Quaternion.Euler(rotation);
-            cameraPivotTransform.localRotation = targetRotation;
-
-        }
-
-        private void HandleCameraCollision(float delta) //카메라 레이케스트 충돌
-        {
-            targetPosition = defaultPosition;
-            RaycastHit hit;
-            Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
-            direction.Normalize();
-
-            if (Physics.SphereCast(cameraPivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(targetPosition), ignoreLayers))
-            {
-                float dis = Vector3.Distance(cameraPivotTransform.position, hit.point);
-                targetPosition = -(dis - cameraCollisionOffset);
-            }
-
-            if(Mathf.Abs(targetPosition) < minimumCollisionOffset)
-            {
-                targetPosition = -minimumCollisionOffset;
-            }
-
-            cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.2f);
-            cameraTransform.localPosition = cameraTransformPosition;
-        }
+        DontDestroyOnLoad(this.gameObject);
+        singleton = this;
+        myTransform = transform;
+        defaultPosition = cameraTransform.localPosition.z;
+        ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+        targetTransform = FindObjectOfType<PlayerManager>().transform;
     }
+
+    public void FollowTarget(float delta) //카메라가 플레이어를 따라감
+    {
+        //Vector3 targetPosition = Vector3.Lerp(myTransform.position, targetTransform.position, delta / followSpeed);
+        Vector3 targetPosition = Vector3.SmoothDamp(myTransform.position, targetTransform.position, ref cameraFollowVelocity, delta / followSpeed);
+        myTransform.position = targetPosition;
+
+        HandleCameraCollision(delta);
+    }
+
+    public void HandleCameraRotation(float delta, float mouseXInput, float mouseYInput) //카메라 회전
+    {
+        lookAngle += (mouseXInput * lookSpeed) / delta;
+        pivotAngle -= (mouseYInput * pivotSpeed) / delta;
+        pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = lookAngle;
+        Quaternion targetRotation = Quaternion.Euler(rotation);
+        myTransform.rotation = targetRotation;
+
+        rotation = Vector3.zero;
+        rotation.x = pivotAngle;
+
+        targetRotation = Quaternion.Euler(rotation);
+        cameraPivotTransform.localRotation = targetRotation;
+
+    }
+
+    private void HandleCameraCollision(float delta) //카메라 레이케스트 충돌
+    {
+        targetPosition = defaultPosition;
+        RaycastHit hit;
+        Vector3 direction = cameraTransform.position - cameraPivotTransform.position;
+        direction.Normalize();
+
+        if (Physics.SphereCast(cameraPivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(targetPosition), ignoreLayers))
+        {
+            float dis = Vector3.Distance(cameraPivotTransform.position, hit.point);
+            targetPosition = -(dis - cameraCollisionOffset);
+        }
+
+        if (Mathf.Abs(targetPosition) < minimumCollisionOffset)
+        {
+            targetPosition = -minimumCollisionOffset;
+        }
+
+        cameraTransformPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, delta / 0.2f);
+        cameraTransform.localPosition = cameraTransformPosition;
+    }
+}
 
