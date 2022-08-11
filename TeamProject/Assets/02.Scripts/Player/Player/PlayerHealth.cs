8787b1667fa617ed484f14d0a92b5db7b87a5891
fufkgj <homin0928@gmail.com>
2022-08-11 18:00:09 +0900

    나무 배치

--- TeamProject/Assets/02.Scripts/Player/Player/PlayerHealth.cs
+++ TeamProject/Assets/02.Scripts/Player/Player/PlayerHealth.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] float maxHp;
-    [SerializeField] float curHp;
+    public float curHp;
 
     [SerializeField] private AudioClip[] HitClips;
     [SerializeField] private AudioClip[] DieClips;
@@ -13,17 +13,17 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 
     public float nextHit = 2.0f;
     BGMController bgmController;
-
+    PlayerLocomotion playerLocomotion;
     AnimatorHandler animatorHandler;
     GameObject Point;
     void Awake()
     {
         animatorHandler = GetComponentInChildren<AnimatorHandler>();
-
         HitClips = Resources.LoadAll<AudioClip>("HitSound");
         DieClips = Resources.LoadAll<AudioClip>("DieSound");
         audioSource = GetComponent<AudioSource>();
         bgmController = GetComponentInChildren<BGMController>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
         Point = GameObject.Find("StartPoint");
     }
 
@@ -61,12 +61,14 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     {
         animatorHandler.PlayTargetAnimation("Player_Death", true);
         GetComponent<CapsuleCollider>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
         this.gameObject.tag = "Untagged";
         yield return new WaitForSeconds(5f);
         GetComponent<CapsuleCollider>().enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
         this.gameObject.tag = "PLAYER";
         animatorHandler.PlayTargetAnimation("Player_Death", false);
-        animatorHandler.PlayTargetAnimation("Locomotion", true);
+        animatorHandler.Initialize();
         bgmController.EnemyCount = 0;
         SetHp(maxHp);
         SceneManager.LoadScene("Field");
