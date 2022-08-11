58dbfe8fdbe5baf15e1760d18cf6764ec539e001
HongSeungyun <97865468+Jerryteacher@users.noreply.github.com>
2022-08-11 11:44:33 +0900

    스크립트 삭제

--- TeamProject/Assets/02.Scripts/Player/Player/PlayerStats.cs
+++ TeamProject/Assets/02.Scripts/Player/Player/PlayerStats.cs
@@ -9,6 +9,16 @@ public class PlayerStats
     public string nickname;
     public int Level = 1;
 
+
+    public float healthLevel = 10;
+
+    public float constitution;  // CON 생명력
+    public float strength;      // STR 힘
+    public float dexterity;     // DEX 손재주, 민첩
+    public float curHealth;     // 현재 HP 체력
+    public float maxHealth;     // 최대 HP 체력
+    public float attack;        // ATK 공격력
+    public float defence;       // DEF 방어력
     public int exp
     {
         get
@@ -18,19 +28,16 @@ public class PlayerStats
         }
     }
     public int currExp = 0;
-
-
-    void Start()
+    //maxHealth = SetMaxHealthFromHealthLevel();
+    //curHealth = maxHealth;
+    //private float SetMaxHealthFromHealthLevel()
+    //{
+    //    maxHealth = healthLevel * 10;
+    //    return curHealth;
+    //}
+    public void TakeDamage(int damage)
     {
-
+        curHealth = curHealth - damage;
     }
-
-
-
-    void Update()
-    {
-
-    }
-
 
 }
